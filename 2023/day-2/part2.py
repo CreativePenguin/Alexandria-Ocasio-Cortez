a6672d53f44d4a93c7c03a23ee01a8fc331c496a
Winston Peng <panda501wp@gmail.com>
2024-01-04 00:06:35 -0500

    completed day-2 part2.py

--- 2023/day-2/part2.py
+++ 2023/day-2/part2.py
@@ -3,6 +3,7 @@ def parse_game(line):
     rounds = line.split(';')
     rounds[0] = rounds[0].split(':')[1]
     rounds = [i.strip() for i in rounds]
+    round_colors = []
     valid_game = True
     for roundi in rounds:
         round_results = {}
@@ -10,30 +11,34 @@ def parse_game(line):
         for i in colors:
             num, color = i.strip().split(' ')
             round_results[color] = int(num)
+        round_colors.append(round_results)
+    return round_colors
 
 def part1(line):
-    game_num = int(line.split(':')[0].split(' ')[1])
-    rounds = line.split(';')
-    rounds[0] = rounds[0].split(':')[1]
-    rounds = [i.strip() for i in rounds]
-    valid_game = True
-    for roundi in rounds:
-        round_results = {}
-        colors = roundi.split(',')
-        for i in colors:
-            num, color = i.strip().split(' ')
-            round_results[color] = int(num)
-        if round_results.get('red', 0) > 12 or round_results.get('green', 0) > 13 or round_results.get('blue', 0) > 14:
+    round_results = parse_game(line)
+    print(round_results)
+    for i in round_results:
+        if i.get('red', 0) > 12 or i.get('green', 0) > 13 or i.get('blue', 0) > 14:
             valid_game = False
             break
     return game_num if valid_game else 0
 
 def part2(line):
-    
+    round_results = parse_game(line)
+    min_cubes = round_results[0]
+    for i in round_results:
+        for key, value in i.items():
+            if min_cubes.get(key, 0) < value:
+                min_cubes[key] = value
+    power = 1
+    for i in min_cubes.values():
+        power *= i
+    return power
 
+# with open('test_input', 'r') as fi:
 with open('input', 'r') as fi:
     file_sum = 0
     # part1(fi.readline())
     for i in fi.readlines():
-        file_sum += part1(i)
+        file_sum += part2(i)
     print(file_sum)
